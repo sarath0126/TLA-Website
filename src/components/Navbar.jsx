a0b1749e18e09fab71ba172398693bd9e5f76591
sarath chandra reddy <sarathchandraramireddy@gmail.com>
2025-10-18 23:48:41 +0530

    Added Responsiveness Better

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/header_logo.svg";
 import "./Navbar.css";
 import { ArrowUpRight } from "lucide-react";
 
 const Navbar = () => {
-  return (
-    <>
-      <div className="navbar">
-     
-     <div className= "logo">
-    <img src={logo} alt="terralogiclogo.svg" />
-     </div>
-          
-      
-        <ul>
-          <li>Our Programs</li>
-          <li>Blogs</li>
-          <li>About us</li>
-        </ul>
+  const [isOpen, setIsOpen] = useState(false);
 
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+
+      {/* Burger button */}
+      <div className={`burger ${isOpen ? "toggle" : ""}`} onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      {/* Nav links */}
+      <ul className={isOpen ? "nav-links active" : "nav-links"}>
+        <li>Our Programs</li>
+        <li>Blogs</li>
+        <li>About Us</li>
+        <li onClick={() => setIsOpen(false)}>Home</li>
         <button>
           Register Now <ArrowUpRight />
         </button>
-      </div>
-    </>
+        
+      </ul>
+    </nav>
   );
 };
 
